c9a07773b34762db92a7f4c195de90c58f7c9eaf
witflash <witflash@gmail.com>
2018-03-13 08:50:42 +0200

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ const config = {
     img: 'src/img',
   },
   dest: {
-    root: 'dist/',
-    css: 'dist/css/',
-    js: 'dist/js/',
-    img: 'dist/img/',
+    root: 'dist',
+    css: 'dist/css',
+    js: 'dist/js',
+    img: 'dist/img',
   },
 };
 
@@ -38,57 +38,43 @@ gulp.task('sass', () => gulp.src(`${config.src.root}/**/*.scss`)
   .pipe(autoprefixer(['last 2 versions'], {cascade:true }))
   .pipe(gulp.dest(config.dest.css)));
 
-gulp.task('css-min', ['scss'], () => {
-  return gulp.src(config.dest.css + '*.css')
+gulp.task('css-min', ['scss'], () => gulp.src(`${config.dest.css}/*.css`)
     .pipe(cssnano())
     .pipe(rename({suffix: '.min'})) 
-    .pipe(gulp.dest(config.dest.css));
-});
+    .pipe(gulp.dest(config.dest.css)));
 
-gulp.task('img', () => {
-  return gulp.src(config.src.img + '**/*')
+gulp.task('img', () => gulp.src(`${config.src.img}/**/*`)
     .pipe(imagemin({
       interlaced: true,
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()]
     }))
-    .pipe(gulp.dest(config.dest.img));
-});
+    .pipe(gulp.dest(config.dest.img)));
 
-gulp.task('js', () => {
-  return gulp.src(config.src.js + '**/*.js')
+gulp.task('js', () => gulp.src(`${config.src.js}/**/*.js`)
     .pipe(concat('app.js'))
-    .pipe(gulp.dest(config.dest.js));
-});
+    .pipe(gulp.dest(config.dest.js)));
 
-gulp.task('uglify', ['js'], () => {
-  return gulp.src(config.dest.js + '**/*.js')
+gulp.task('uglify', ['js'], () => gulp.src(`${config.dest.js}/**/*.js`)
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(config.dest.js))
-});
+    .pipe(gulp.dest(config.dest.js)));
 
-gulp.task('browser-sync', () => {
-  browserSync({
+gulp.task('browser-sync', () => browserSync({
     server: {
       baseDir: config.dest.root
     },
     notify: true
-  });
-});
+  }));
 
-gulp.task('clean', () => {
-  return del.sync(config.dest.root);
-});
+gulp.task('clean', () => del.sync(config.dest.root));
 
-gulp.task('default', ['browser-sync', 'nunjucks', 'scss', 'js', 'img'], () => {
+gulp.task('default', ['browser-sync', 'nunjucks', 'scss', 'js', 'img'], () =>
   gulp.watch(config.src.html + '**/*.html', ['nunjucks', browserSync.reload]);
   gulp.watch(config.src.sass + '**/*.scss', ['scss', browserSync.reload]);
   gulp.watch(config.src.js + '**/*.js', ['js', browserSync.reload]);
-  gulp.watch(config.src.img + '**/*.*', ['img', browserSync.reload]);
-    
-});
+  gulp.watch(config.src.img + '**/*.*', ['img', browserSync.reload]););
 
 gulp.task('build', ['clean', 'nunjucks', 'scss', 'img', 'css-min', 'js', 'uglify'], () => {
   return
